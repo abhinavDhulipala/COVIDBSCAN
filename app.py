66159351613834c81c8ee86091d86e2a8e99cf13
abhinavDhulipala <46908860+abhinavDhulipala@users.noreply.github.com>
2020-10-12 19:08:44 -0700

    schematic added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 import boto3
 
 app = Flask(__name__)
@@ -6,12 +6,24 @@ app.config.update(
     ENV='development'
 )
 
-db = boto3.client('dynamodb')
+
+# db = boto3.client('dynamodb')
+@app.route("/")
+def redirect_to_login():
+    return redirect("/login")
 
 
-@app.route('/<name>')
-def hello_world():
-    return render_template('index.html')
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        return redirect("/dashboard")
+    return render_template('login.html', error=error)
+
+
+@app.route("/dashboard")
+def do_the_dash():
+    return render_template('main.html')
 
 
 if __name__ == '__main__':

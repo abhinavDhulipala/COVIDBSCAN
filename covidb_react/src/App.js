76babeac3e669f3090de817a520a689e6c30eaf5
abhinavDhulipala <46908860+abhinavDhulipala@users.noreply.github.com>
2020-10-17 20:38:45 -0700

    Auth Implemented

--- covidb_react/src/App.js
+++ covidb_react/src/App.js
@@ -1,30 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
 import './App.css';
+import {Button, Card, Container, Form} from "react-bootstrap";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {withAuthenticator, AmplifySignOut} from "@aws-amplify/ui-react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 Amplify.configure(awsExports);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <Container className="mw-50">
+                        <Button> <AmplifySignOut/> </Button>
+                        <Card>
+                            <Card.Body>
+                                <Card.Text className="text-info">
+                                    Congrats! you are logged in
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Container>
+                </header>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default withAuthenticator(App, true);

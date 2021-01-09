33b02c04a44cb6da503e75af5e1d06c6aac74d3a
abhinavDhulipala <46908860+abhinavDhulipala@users.noreply.github.com>
2021-01-08 17:03:26 -0800

    [refactor] Navbar refactored to use proper routes

--- cohack/src/App.js
+++ cohack/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import NavBar from "./components/NavBar";
-import HomePage from "./components/HomePage";
 
 
 const theme = {
--- cohack/src/components/NavBar.js
+++ cohack/src/components/NavBar.js
@@ -5,22 +5,20 @@ import {Nav, Navbar} from "react-bootstrap";
 class NavBar extends Component {
     render() {
         return <Navbar bg="light" variant="light">
-            <Navbar.Brand href="#home" >
+            <Navbar.Brand href="/" >
                 <img
                     alt=""
                     src={require('../images/covidLogo.png')}
                     width="50"
                     height="50"
                     className="d-inline-block align-top"
-                    onClick={() => window.location.assign('/')}
                 />
             </Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link href={process.env.REACT_APP_ABOUT}> About </Nav.Link>
-                <Nav.Link href="#features"> Map </Nav.Link>
-                <Nav.Link href="#pricing" onSelect={() => alert('No <3')}> Don't Contact Us </Nav.Link>
+                <Nav.Link href="/map">Map</Nav.Link>
+                <Nav.Link> Don't Contact Us </Nav.Link>
             </Nav>
-
         </Navbar>
     }
 }
